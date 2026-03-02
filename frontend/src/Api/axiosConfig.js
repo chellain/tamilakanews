fe5ec78705469b42247fa62d21d2f2d175d86eda
Gowtham <717823s118@kce.ac.in>
2026-03-02 16:58:46 +0530

    the first one

---
+++ frontend/src/Api/axiosConfig.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL || "/api",
+});
+
+// Attach JWT token when available
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("userToken");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default api;
